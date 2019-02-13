3c665359e96682dd0ba5a69e1f915eb991543927
interestingLSY <2659723130@qq.com>
2019-02-13 16:46:34 +0800

    Small Fix

--- web/static/footer.js
+++ web/static/footer.js
@@ -1,5 +1,5 @@
 function Footer_position(){
-	if( $(document.body).height() < $(window).height()-30 ){
+	if( $(document).height() < $(window).height()-30 ){
 		$("#footer").css("position","fixed")
 		$("#footer").css("bottom","0px")
 	}else{
