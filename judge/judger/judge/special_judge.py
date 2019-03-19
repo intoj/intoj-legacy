4aaf134bc0a34f588ac44d042c5739e1dc2e3b7f
interestingLSY <2659723130@qq.com>
2019-03-19 20:45:31 +0800

    spj 可以读取 std_answer ( 当时怎么想的...

--- judge/judger/judge/special_judge.py
+++ judge/judger/judge/special_judge.py
@@ -9,6 +9,7 @@ spj_time_limit = 4000
 spj_memory_limit = 512
 
 def Special_Judge( inputpath, outputpath, anspath, fullscore, spj_exepath ):
+	os.system('cp %s ../tmp/std_answer'%anspath)
 	(status,_unused_mes) = subprocess.getstatusoutput(" \
 		cd ../tmp && lrun \
 		--max-real-time %f \
@@ -21,8 +22,11 @@ def Special_Judge( inputpath, outputpath, anspath, fullscore, spj_exepath ):
 	with open('../tmp/spj_output','r') as spj_score_file:
 		index = spj_score_file.readlines()
 		try:
+			index[0] = index[0].strip()
+			index[1] = index[1].strip()
 			status = int(index[0])
 			score = float(index[1])/100 * fullscore
+			if not index[1].isdigit(): raise ValueError()
 		except:
 			return 2, 0, 'spj 输出的分数不对: \n%s'%str(index)
 	with open('../tmp/spj_message','r') as spj_message_file:
