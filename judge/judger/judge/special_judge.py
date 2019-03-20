05b1299d534d7c339f359fe24c92b18098886ea8
interestingLSY <2659723130@qq.com>
2019-03-20 09:25:59 +0800

    题目列表里的分数加入 DynamicSize

--- judge/judger/judge/special_judge.py
+++ judge/judger/judge/special_judge.py
@@ -27,7 +27,7 @@ def Special_Judge( inputpath, outputpath, anspath, fullscore, spj_exepath ):
 			index[1] = index[1].strip()
 			status = int(index[0])
 			score = float(index[1])/100 * fullscore
-			if not modules.Is_Number(score): raise ValueError()
+			if not modules.Is_Number(str(score)): raise ValueError()
 		except:
 			return 2, 0, 'spj 输出的分数不对: \n%s'%str(index)
 	with open('../tmp/spj_message','r') as spj_message_file:
