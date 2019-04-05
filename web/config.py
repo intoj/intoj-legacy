839751cb4c36873c92b3ff050abf8323bb54f6eb
interestingLSY <2659723130@qq.com>
2019-04-05 11:36:34 +0800

    Fix ??? in config file

--- web/config.py
+++ web/config.py
@@ -3,11 +3,8 @@ import gevent.monkey
 gevent.monkey.patch_all()
 import multiprocessing
 
-debug = True
-loglevel = 'debug'
 bind = '0.0.0.0:5000'
 pidfile = '.log/gunicorn.pid'
-logfile = '.log/debug.log'
 
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'gunicorn.workers.ggevent.GeventWorker'
